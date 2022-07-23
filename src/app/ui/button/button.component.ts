258566d033d3f1c1cbdfd54563703c8b8dfc9fa9
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2022-07-22 21:04:09 -0400

    feat (ui): button

---
+++ src/app/ui/button/button.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+
+type ButtonType = 'primary' | 'secondary';
+type ButtonSize = 'small' | 'medium';
+
+@Component({
+  selector: 'ui-button',
+  template: `
+    <button #button [type]="type" (click)="onClick($event)">
+    	<ng-content></ng-content>
+    </button>`,
+  styles: [`
+    button {
+        aspect-ratio: 1;
+        border-radius: 50%;
+        border: 0px;
+        color: var(--white);
+        outline: none;
+        background: transparent;
+        transition: background-color 0.3s;
+    }
+    button:hover {
+        background-color: var(--white-30);
+    }`],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ButtonComponent implements OnInit, AfterViewInit {
+
+  @Input() type: ButtonType = 'primary';
+  @Input() size: ButtonSize = 'medium';
+
+  @Output() click = new EventEmitter<Event>();
+
+  @ViewChild('button') button: ElementRef<HTMLButtonElement>;
+
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngAfterViewInit(): void {
+    this.button.nativeElement.style.width = this.dimension;
+    this.button.nativeElement.style.height = this.dimension;
+    this.button.nativeElement.style.backgroundColor = this.background;
+  }
+
+  get dimension() {
+    return {
+      ['small']: '48px',
+      ['medium']: '64px',
+    }[this.size];
+  }
+
+  get background() {
+    return {
+      ['primary']: 'var(--white-10) !important',
+      ['secondary']: 'transparent !important',
+    }[this.type];
+  }
+
+  onClick(event: Event) {
+    this.click.emit(event);
+  }
+}
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // Component
 import { CursorComponent } from './cursor/cursor.component';
-import { TypographyDirective } from './typography.directive';
+import { TypographyDirective } from './typography/typography.directive';
 import { ProgressComponent } from './progress/progress.component';
+import { ButtonComponent } from './button/button.component';
 
 
 
@@ -16,11 +17,13 @@ import { ProgressComponent } from './progress/progress.component';
     CursorComponent,
     ProgressComponent,
     TypographyDirective,
+    ButtonComponent,
   ],
   exports: [
     CursorComponent,
     ProgressComponent,
     TypographyDirective,
+    ButtonComponent,
   ],
 })
 export class UiModule { }
