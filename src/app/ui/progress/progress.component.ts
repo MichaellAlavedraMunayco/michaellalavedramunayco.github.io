5dfa4bed3dc40171edc8779b753ee9ab61e604a5
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2022-07-22 20:23:14 -0400

    feat: progress

---
+++ src/app/ui/progress/progress.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input, OnChanges, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'ui-progress',
+  template: `
+    <div class="progress" [style]="'width:' + _percent + '%'"></div>`,
+  styles: [`
+    :host {
+        position: relative;
+        height: 4px;
+        width: 100%;
+        min-height: 4px;
+        min-width: 30px;
+        background-color: var(--white-50);
+        opacity: 1;
+        transition: opacity 0.3s;
+    }
+    :host.hide {
+        opacity: 0;
+    }
+    div.progress {
+        position: absolute;
+        inset: 0;
+        right: unset;
+        width: 0%;
+        background-color: var(--white);
+        transition: width .05s;
+    }`],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProgressComponent implements OnInit, OnChanges {
+
+  @Input() set percent(percent: number) {
+
+    percent = percent || 0;
+    percent = percent < 0 ? 0 : percent;
+    percent = percent > 100 ? 100 : percent;
+    this._percent = percent;
+
+    if (this.hideOnCompletion && this._percent === 100) {
+
+      window.setTimeout(() => {
+        this.elementRef.nativeElement.className = 'hide';
+      }, 100)
+    }
+  }
+
+  @Input() hideOnCompletion: boolean = true;
+
+  _percent: number = 100;
+
+
+  constructor(private elementRef: ElementRef<HTMLElement>) { }
+
+  ngOnInit(): void { }
+
+  ngOnChanges(): void { }
+
+}
