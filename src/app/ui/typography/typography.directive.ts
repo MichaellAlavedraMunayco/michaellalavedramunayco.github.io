c9dd68c514bf53440e44114c335a9227bdc97635
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2022-07-23 12:47:17 -0400

    feat: font size responsive

--- src/app/ui/typography/typography.directive.ts
+++ src/app/ui/typography/typography.directive.ts
@@ -24,18 +24,18 @@ export class TypographyDirective implements AfterViewInit {
   get fontSize() {
     return {
       ['display']: {
-        ['extralarge']: `${128 / this.sizeBase}rem`,
-        ['large']: `${80 / this.sizeBase}rem`,
-        ['medium']: `${48 / this.sizeBase}rem`,
-        ['small']: `${36 / this.sizeBase}rem`,
-        ['extrasmall']: `${30 / this.sizeBase}rem`,
+        ['extralarge']: `${72 / this.sizeBase}em`,
+        ['large']: `${60 / this.sizeBase}em`,
+        ['medium']: `${48 / this.sizeBase}em`,
+        ['small']: `${36 / this.sizeBase}em`,
+        ['extrasmall']: `${30 / this.sizeBase}em`,
       },
       ['text']: {
-        ['extralarge']: `${20 / this.sizeBase}rem`,
-        ['large']: `${18 / this.sizeBase}rem`,
-        ['medium']: `${16 / this.sizeBase}rem`,
-        ['small']: `${14 / this.sizeBase}rem`,
-        ['extrasmall']: `${12 / this.sizeBase}rem`,
+        ['extralarge']: `${20 / this.sizeBase}em`,
+        ['large']: `${18 / this.sizeBase}em`,
+        ['medium']: `${16 / this.sizeBase}em`,
+        ['small']: `${14 / this.sizeBase}em`,
+        ['extrasmall']: `${12 / this.sizeBase}em`,
       }
     }[this.type][this.size];
   }
