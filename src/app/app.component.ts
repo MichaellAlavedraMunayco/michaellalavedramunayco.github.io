4281abdc0074fcc2281e43c78156c65e6a8c035c
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2022-07-23 20:05:35 -0400

    feat: order folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 // rxjs
 import { fromEvent, merge, Subscription } from 'rxjs';
@@ -7,137 +7,9 @@ import { debounceTime, distinct, distinctUntilChanged, filter } from 'rxjs/opera
 import { CertificationsPageComponent } from './pages/certifications-page/certifications-page.component';
 import { OverviewPageComponent } from './pages/overview-page/overview-page.component';
 import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+// models
+import { Page, PageController, PageEvent } from './core/models/page-component';
 
-type Path = 'overview' | 'projects' | 'certifications';
-type PageComponent = OverviewPageComponent | ProjectsPageComponent | CertificationsPageComponent;
-type PageEvent = 'onprevious' | 'onnext';
-type PageParams = { url?: string, scrollRef?: number, deltaY?: number, key?: string };
-
-class Page {
-
-  scrollRef: number;
-  path: Path;
-  title: string;
-  component: PageComponent;
-  previousPage: Page;
-  nextPage: Page;
-
-  constructor({ path, title, component }: Partial<Page>) {
-    this.path = path;
-    this.title = title;
-    this.component = component;
-    this.scrollRef = this.getScrollRef();
-  }
-
-
-  getScrollRef(): number {
-    return this.component.getBoundingClientRect().y;
-  }
-
-  updateScrollRef() {
-    this.scrollRef = this.getScrollRef();
-  }
-
-  setSiblingPages(previous: Page, next: Page) {
-    this.previousPage = previous;
-    this.nextPage = next;
-  }
-
-  scrollIntoView() {
-    this.component.scrollIntoView();
-  }
-
-  setTitle() {
-    document.title = this.title;
-  }
-}
-
-class PageController {
-
-  page: Page;
-  private event: PageEvent = 'onprevious';
-  private stack: Page[] = [];
-
-  lastScrollRef: number = 0;
-
-  constructor() { }
-
-  setPage(page: Page) {
-    this.page = page;
-  }
-
-  setEvent(event: PageEvent) {
-    this.event = event;
-  }
-
-  setStack(stack: Page[]) {
-    this.stack = stack;
-  }
-
-  toPath(url: string): Path {
-    const path = url.replace('/', '');
-    return ['projects', 'certifications'].includes(path)
-      ? path as Path
-      : 'overview';
-  }
-
-  findPage({ url, scrollRef }: PageParams): Page {
-    if (url) {
-      const path = this.toPath(url);
-      return this.stack.find(page => page.path === path);
-    }
-    if (scrollRef)
-      return this.stack.find(page => page.scrollRef === scrollRef);
-  }
-
-  getEventBy({ scrollRef, key, deltaY }: PageParams): PageEvent {
-
-    if (scrollRef)
-      return scrollRef > this.lastScrollRef ? 'onnext' : 'onprevious';
-
-    if (deltaY)
-      return deltaY > 0 ? 'onnext' : 'onprevious';
-
-    if (key)
-      return {
-        'ArrowDown': 'onnext',
-        'ArrowUp': 'onprevious'
-      }[key] as PageEvent;
-  }
-
-  navigate() {
-
-    if (!this.page) return;
-
-    this.updatePagesOrder();
-
-    this.page.setTitle();
-    this.page.scrollIntoView();
-  }
-
-  private updatePagesOrder() {
-
-    if (!this.event) return;
-
-    if (this.event === 'onprevious') {
-
-      const lastPage = this.stack.pop();
-      this.stack.unshift(lastPage);
-
-      lastPage.component.moveToFirst();
-    }
-
-    if (this.event === 'onnext') {
-
-      const firstPage = this.stack.shift();
-      this.stack.push(firstPage);
-
-      firstPage.component.moveToLast();
-    }
-
-  }
-
-}
 
 @Component({
   selector: 'app-root',
@@ -232,10 +104,7 @@ export class AppComponent implements AfterViewInit, OnDestroy {
 
   onGoToPage(pageEvent: PageEvent) {
 
-    const page = {
-      ['onprevious']: this.pageController.page.previousPage,
-      ['onnext']: this.pageController.page.nextPage,
-    }[pageEvent];
+    const page = this.pageController.getPageFromEvent(pageEvent);
 
     this.pageController.setEvent(pageEvent);
 
